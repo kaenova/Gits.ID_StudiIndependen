f6d9d6c70f9aa02d831ff587c08c6d88409aa1f6
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-09-03 17:39:03 +0700

    Modul 3 :: Memperbaiki currency format

--- Modul 3/currencyFormat.go
+++ Modul 3/currencyFormat.go
@@ -10,11 +10,11 @@ import (
 	"strconv"
 )
 
-func main(){
+func main() {
 
 	var (
-		nilai, mod int
-		final string
+		nilai, mod              int
+		final                   string
 		minus, first_conversion bool
 	)
 	// Currency Initialization
@@ -33,32 +33,39 @@ func main(){
 	}
 
 	// Konversi nilai
-	for nilai > 0 {
-		mod = nilai % 1000
-		nilai = nilai / 1000
+	if nilai > 0 {
 
-		if first_conversion {
-			first_conversion = false
-		} else {
-			final = "."+final
-		}
+		for nilai > 0 {
+			mod = nilai % 1000
+			nilai = nilai / 1000
 
-		if mod == 0 {
-			final = "000" + final
-		} else if mod < 10 {
-			// Kurang dari sepuluh
-			final = strconv.Itoa(mod)+final
-			if nilai > 10 {
-				// Kalau misalkan ada yang masih bisa dibagi lagi
-				final = "00" + final
+			if first_conversion {
+				first_conversion = false
+			} else {
+				final = "." + final
 			}
-		} else if mod / 10 < 10 {
-			// Kurang dari 100
-			final = "0"+strconv.Itoa(mod)+final
-		} else {
-			final = strconv.Itoa(mod)+final
-		}
 
+			if mod == 0 {
+				final = "000" + final
+			} else if mod < 10 {
+				// Kurang dari sepuluh
+				final = strconv.Itoa(mod) + final
+				if nilai > 0 {
+					// Kalau misalkan ada yang masih bisa dibagi lagi
+					final = "00" + final
+				}
+			} else if mod/10 < 10 {
+				// Kurang dari 100
+				final = strconv.Itoa(mod) + final
+				if nilai > 0 {
+					final = "0" + final
+				}
+			} else {
+				final = strconv.Itoa(mod) + final
+			}
+		}
+	} else {
+		final = "0" + final
 	}
 
 	// Finalisasi
