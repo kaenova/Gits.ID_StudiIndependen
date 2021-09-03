ea1af057614eef66b840855c58139ed87825adb2
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-09-04 00:20:02 +0700

    Modul 3 :: Menambahkan adanya dollar

--- Modul 3/currencyFormat.go
+++ Modul 3/currencyFormat.go
@@ -13,18 +13,20 @@ import (
 func main() {
 
 	var (
-		nilai, mod              int
-		final                   string
-		minus, first_conversion bool
+		nilai, input, mod          int
+		final_rupiah, final_dollar string
+		minus, first_conversion    bool
 	)
 	// Currency Initialization
 	minus = false
-	final = ",00"
+	final_rupiah = ",00"
 	first_conversion = true
 
 	//Input
 	fmt.Print("Masukkan jumlah nilai : ")
-	fmt.Scanf("%d", &nilai)
+	fmt.Scanf("%d", &input)
+
+	nilai = input
 
 	// Minus check
 	if nilai < 0 {
@@ -32,7 +34,7 @@ func main() {
 		nilai = nilai * -1
 	}
 
-	// Konversi nilai
+	// Konversi nilai rupiah
 	if nilai > 0 {
 
 		for nilai > 0 {
@@ -42,38 +44,119 @@ func main() {
 			if first_conversion {
 				first_conversion = false
 			} else {
-				final = "." + final
+				final_rupiah = "." + final_rupiah
 			}
 
 			if mod == 0 {
-				final = "000" + final
+				final_rupiah = "000" + final_rupiah
 			} else if mod < 10 {
 				// Kurang dari sepuluh
-				final = strconv.Itoa(mod) + final
+				final_rupiah = strconv.Itoa(mod) + final_rupiah
 				if nilai > 0 {
 					// Kalau misalkan ada yang masih bisa dibagi lagi
-					final = "00" + final
+					final_rupiah = "00" + final_rupiah
 				}
 			} else if mod/10 < 10 {
 				// Kurang dari 100
-				final = strconv.Itoa(mod) + final
+				final_rupiah = strconv.Itoa(mod) + final_rupiah
 				if nilai > 0 {
-					final = "0" + final
+					final_rupiah = "0" + final_rupiah
 				}
 			} else {
-				final = strconv.Itoa(mod) + final
+				final_rupiah = strconv.Itoa(mod) + final_rupiah
 			}
 		}
 	} else {
-		final = "0" + final
+		final_rupiah = "0" + final_rupiah
 	}
 
 	// Finalisasi
-	final = "Rp " + final
+	final_rupiah = "Rp " + final_rupiah
 	if minus {
-		final = "- " + final
+		final_rupiah = "- " + final_rupiah
 	}
 
-	// Output
-	fmt.Println("Hasil konversi :", final)
+	// Output Rupiah
+	fmt.Println("Hasil konversi Rupiah :", final_rupiah)
+
+	// Konversi ke Dollar
+	fmt.Println("Dengan nilai tukar Dolar ke Rupiah 1 USD = Rp 14.000.000,00")
+
+	// Buat menjadi 2 precision
+	var nilai_dollar, err = strconv.ParseFloat(fmt.Sprintf("%.2f", float32(input)/14000.0), 2)
+
+	if err == nil {
+		final_dollar = ""
+
+		// Check apakah ada koma?
+		nilai_koma := int(nilai_dollar*100.0) % 100
+		koma := true
+		if nilai_koma == 0 {
+			koma = false
+		}
+
+		if koma {
+			if nilai_koma < 10 {
+				final_dollar = ",0" + strconv.Itoa(nilai_koma)
+			} else {
+				final_dollar = "," + strconv.Itoa(nilai_koma)
+			}
+		} else {
+			final_dollar = ",00"
+		}
+
+		var selain_koma_dollar int = int(nilai_dollar)
+
+		first_conversion = true
+
+		//Konversi nilai selain koma ke dollar
+		if selain_koma_dollar > 0 {
+
+			for selain_koma_dollar > 0 {
+				mod = selain_koma_dollar % 1000
+				selain_koma_dollar = selain_koma_dollar / 1000
+
+				if first_conversion {
+					first_conversion = false
+				} else {
+					final_dollar = "." + final_dollar
+				}
+
+				if mod == 0 {
+					final_dollar = "000" + final_dollar
+				} else if mod < 10 {
+					// Kurang dari sepuluh
+					final_dollar = strconv.Itoa(mod) + final_dollar
+					if selain_koma_dollar > 0 {
+						// Kalau misalkan ada yang masih bisa dibagi lagi
+						final_dollar = "00" + final_dollar
+					}
+				} else if mod/10 < 10 {
+					// Kurang dari 100
+					final_dollar = strconv.Itoa(mod) + final_dollar
+					if selain_koma_dollar > 0 {
+						final_dollar = "0" + final_dollar
+					}
+				} else {
+					final_dollar = strconv.Itoa(mod) + final_dollar
+				}
+			}
+		} else {
+			final_dollar = "0" + final_dollar
+		}
+
+		// Finalisasi
+		final_dollar = "$ " + final_dollar
+		if minus {
+			final_dollar = "- " + final_dollar
+		}
+
+		// Output
+		fmt.Println("Hasil konversi Dollar :", final_dollar)
+
+	} else {
+		// Panic saat mengubah menjadi 2 decimal placess
+		fmt.Println("Error!")
+	}
+
 }
---
+++ Modul 3/percobaanFloat.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a float32 = 3.2
+	var b int = int(a)
+
+	fmt.Println(b)
+}
