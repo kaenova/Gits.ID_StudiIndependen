d343644ab3b37b60a81bc011c1f65afa707b378b
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-09-17 10:30:36 +0700

    Modul 11 :: mengerjakan Tugas Interface

--- Modul 11/Interface.go
+++ Modul 11/Interface.go
@@ -6,44 +6,44 @@ import (
 )
 
 // Defining for Persegi
-type Persegi struct {
+type PersegiPanjnag struct {
 	panjang float32
 	lebar   float32
 }
 
-func (p Persegi) Keliling() float32 {
+func (p PersegiPanjnag) Keliling() float32 {
 	return (2 * p.panjang) + (2 * p.lebar)
 }
 
-func (p Persegi) Luas() float32 {
+func (p PersegiPanjnag) Luas() float32 {
 	return p.panjang * p.lebar
 }
 
 // Defining for Segitiga Sama sisi
 type Segitiga struct {
-	alas   float32
-	tinggi float32
+	sisi float32
 	/*
-		Ini kalau bisa ke inisialisasiin berdasarkan nilai alas. Bagaimana ya kak caranya?
+		Ini kalau bisa ke inisialisasiin berdasarkan nilai alas.
+		Bagaimana ya kak caranya?
 		Jadi di segitiganya hanya masukkin 1 nilai saja.
 	*/
 }
 
-func (s Segitiga) init() {
-	// Ini hanya percobaan
-	setengah_alas_kuadrat := math.Pow(float64(0.5*s.alas), 2)
-	alas_kuadrat := math.Pow(float64(0.5*s.alas), 2)
-	s.tinggi = float32(
-		math.Pow((float64(alas_kuadrat - setengah_alas_kuadrat)), 0.5))
+func (s Segitiga) Tinggi() float32 {
+	setengah_alas_kuadrat := math.Pow(float64(0.5*s.sisi), 2)
+	alas_kuadrat := math.Pow(float64(s.sisi), 2)
+
+	return float32(
+		math.Pow(float64(alas_kuadrat-setengah_alas_kuadrat), 0.5))
 }
 
 func (s Segitiga) Keliling() float32 {
 	// Belum selesai dibuat
-	return 0
+	return s.sisi * 3
 }
 
 func (s Segitiga) Luas() float32 {
-	return 0.5 * s.alas * s.tinggi
+	return 0.5 * s.sisi * s.Tinggi()
 }
 
 // Making interface
@@ -53,6 +53,15 @@ type Hitung interface {
 }
 
 func main() {
-	s := Segitiga{1, 0}
-	fmt.Println(s.tinggi)
+	var bangunDatar Hitung
+
+	fmt.Printf("=== %-20s\n", "Persegi Panjang")
+	bangunDatar = PersegiPanjnag{panjang: 10, lebar: 5}
+	fmt.Printf("%10s : %.2f\n", "Keliling", bangunDatar.Keliling())
+	fmt.Printf("%10s : %.2f\n", "Luas", bangunDatar.Luas())
+
+	fmt.Printf("=== %-20s\n", "Segitiga Sama Sisi")
+	bangunDatar = Segitiga{sisi: 10}
+	fmt.Printf("%10s : %.2f\n", "Keliling", bangunDatar.Keliling())
+	fmt.Printf("%10s : %.2f\n", "Luas", bangunDatar.Luas())
 }
